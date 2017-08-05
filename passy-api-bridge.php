692d4c29107e5842e92e32146f215604811ee23e
Liz3(Yann HN) <accounts@liz3.de>
2017-08-05 14:04:36 +0200

    Update passy-api-bridge.php

--- passy-api-bridge.php
+++ passy-api-bridge.php
@@ -1,12 +1,9 @@
 <?php
-
 $target_address = "https://app.passy.pw/action.php";
 $content_type = $_SERVER["CONTENT_TYPE"];
 $method = $_SERVER['REQUEST_METHOD'];
 $content = null;
-
 if($method == "OPTIONS") {
-
    header("Access-Control-Request-Headers: authorization,content-type");
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: authorization,Access-Control-Allow-Origin,Content-Type,X-Requested-With");
@@ -14,16 +11,15 @@ if($method == "OPTIONS") {
     die();
 }
 if(isset($_POST["target"])) $target_address = $_POST["target"];
-
-
 $passy_request = curl_init($target_address);
 curl_setopt($passy_request, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($passy_request, CURLOPT_VERBOSE, 1);
 curl_setopt($passy_request, CURLOPT_HEADER, 1);
+curl_setopt($ch,CURLOPT_USERAGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36");
+
 $access_cookie = "";
 if($method == "POST") {
     $request_args = array();
-
     foreach ($_POST as $key => $value) {
         if($key == "access_token") {
             $access_cookie = $value;
@@ -51,10 +47,13 @@ foreach (explode("\n", $header) as $item) {
         $x = true;
         continue;
     }
+##PHPSESSID
   if(strpos($item, "SCRUMPLEX") !== false) {
         array_push($tokens, substr(explode(";", $item)[0], 12));
-      }
+  } else if(strpos($item, "PHPSESSID") !== false) {
+	        array_push($tokens, substr(explode(";", $item)[0], 12));
 
+}
 }
 header("Access-Control-Allow-Origin: *");
 header("Content-type: application/json");
